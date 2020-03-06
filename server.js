516d12d0282b1e6a52a829bdf52643c7222f33b2
Joscana-Garcia <59126118+joscana@users.noreply.github.com>
2020-03-06 12:05:15 -0500

    Starter code

---
+++ server.js
@@ -0,0 +1,41 @@
+//Express to run server and routes
+const express = require('express');
+
+//Start up an instance of app
+const app = express();
+
+/*Dependencies */
+const bodyParser = require('body-parser')
+
+/*Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded ({ extended: false}));
+app.use(bodyParser.json());
+//Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+//Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 8000;
+//Spin up the server
+const server = app.listen(port, listening);
+//const server = app.listen(port, ()=>{console.log{`running on localhost: ${port}`)})
+//Callback to debug
+function listening(){
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+}
+
+
+/* Empty JS object to act as endpoint for all routes */
+const projectData = {};
+
+
+//Creates a GET route that uses the url /all and returns JavaScript object named projectData
+app.get('/all', sendData);
+
+function sendData(request, response) {
+    response.send(projectData);
+};
